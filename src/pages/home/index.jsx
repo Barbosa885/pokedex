d143c472cf3e25793a98f0c0829117392c666a67
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-10-09 22:49:41 -0300

    :sparkles:Implements the logic to the pokemon images

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,32 +3,30 @@ import React, { useState, useEffect } from "react";
 import * as styled from "./styles";
 
 import api from "../../services/api";
+import axios from "axios";
 
 import Card from "../../components/card";
 
 const Home = () => {
   const [pokemon, setPokemon] = useState([]);
-  
+
   useEffect(() => {
     getPokemon();
   }, []);
 
   const getPokemon = async () => {
-    await api
-      .get("/pokemon?limit=150")
-      .then((response) => {
-        setPokemon(response.data.results);
-      })
-      .catch((error) => {
-        console.log(error);
-      }
-    );
+    var endpoints = [];
+    for (var id = 1; id <= 150; id++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    }
+    var response = axios.all(endpoints.map((endpoint) => api.get(endpoint))).then((res) => setPokemon(res)).catch((err) => console.log("Error" + err));
+    return await response;
   };
 
   return (
     <styled.Container>
-      {pokemon.map((poke) => ( 
-        <Card key={poke.name} name={poke.name} />
+      {pokemon.map((poke) => (
+        <Card key={poke.name} name={poke.data.name} image={poke.data.sprites.front_default} />
       ))}
     </styled.Container>
   );
